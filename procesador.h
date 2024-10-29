d3b1d4264f1903a48b9a8903ec65995f351763d2
SAngel_007 <larapatriciorobertoangel@gmail.com>
2024-10-29 00:25:10 -0600

    Update procesador.h
    Se actualizo un poco el .h para que quede mas claro

--- procesador.h
+++ procesador.h
@@ -1,34 +1,39 @@
+#ifndef PROCESADOR_H
+#define PROCESADOR_H
 
 #include <stdbool.h>
+
 #define MEM_SIZE 256
-//const MEM_SIZE = 256; preprocesador y lenguaje c
 #define MAX_INSTRUCTIONS 100
 #define NUM_REGISTERS 2
 #define NUM_FLAGS 2
-// Definimos para las banderas.
-#define ZF 0 
+
+// Definimos los índices para las banderas
+#define ZF 0
 #define NF 1
 
-typedef struct{
-  char inst[6];
-  char op1;
-  int op2;
-}Instruction;
+// Estructura que representa una instrucción
+typedef struct {
+  char inst[6]; // Nombre de la instrucción, con espacio suficiente para almacenar
+  char op1;     // Primer operando, típicamente un registro (e.g., 'A', 'B')
+  int op2;      // Segundo operando, típicamente un valor o dirección
+} Instruction;
+
 // Estructura que representa el procesador
-typedef struct{// Código de los datos especificados
-  int memory[MEM_SIZE];
-  int registers[NUM_REGISTERS];
-  bool flags[NUM_FLAGS];
-  int PC; // Contador de programa.
-  Instruction code_memory[MAX_INSTRUCTIONS]; // Memoria de instrucciones.
-}Proccesor;
+typedef struct {
+  int memory[MEM_SIZE];                      // Memoria de datos
+  int registers[NUM_REGISTERS];              // Registros del procesador
+  bool flags[NUM_FLAGS];                     // Banderas (ZF y NF)
+  int PC;                                    // Contador de programa
+  Instruction code_memory[MAX_INSTRUCTIONS]; // Memoria de instrucciones
+} Processor;
+
 // Funciones del procesador
-/**
-  * Inicializa el procesador.
-  */
-void init_processor (Proccesor *cpu);
-void load_program (Proccesor *cpu, const char *filename);
-void update_flags (Proccesor *cpu, int result);
-void alu_and_control_unity (Proccesor *cpu);
-void print( Proccesor *cpu) ;
-void fprint( Proccesor *cpu) ;
+void init_processor(Processor *cpu); // Inicializa el procesador
+void load_program(Processor *cpu, const char *filename); // Carga un programa en el procesador
+void update_flags(Processor *cpu, int result); // Actualiza las banderas en base a un resultado
+void alu_and_control_unity(Processor *cpu);         // Ejecuta la unidad de control y ALU
+void print(Processor *cpu);  // Imprime el estado del procesador en pantalla
+void fprint(Processor *cpu); // Guarda el estado del procesador en un archivo
+
+#endif
